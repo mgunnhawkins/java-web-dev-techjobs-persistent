f2c62e1e1df2b5886c1403c89cf407443dd6e43e
Melanie Gunn <mgunn589@gmail.com>
2021-10-14 11:41:58 -0500

    completed part 1

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,13 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max=500)
+    private String description;
+
+    public Skill() { }
 }
